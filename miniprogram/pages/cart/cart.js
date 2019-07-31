c38af820366d281dca6e6a858f42e98f329ca3fe
realwangshuxin <1137758203@qq.com>
2019-07-31 16:02:23 +0800

    完成

--- miniprogram/pages/cart/cart.js
+++ miniprogram/pages/cart/cart.js
@@ -1,4 +1,4 @@
-// pages/buy/buy.js
+// pages/cart/cart.js
 const db = wx.cloud.database({
   env: "wangshuxin-78uzn"
 });
@@ -8,51 +8,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-    arr:[],
-    inputValue:"",
-    inputValue2:""
+    count:1,
+    price:398,
+    total:398,
+    result:[],
+    phone:""
   },
-  cart(){
-    if (this.data.inputValue == "") {
-      wx.showToast({
-        title: '请输入手机号',
-        icon: "none",
-        duration: 2000
+  down(){
+    if(this.data.count>1){
+      var count2=this.data.count-1;
+      var total2=count2*this.data.price
+      this.setData({
+        count:count2,
+        total:total2
       })
-      return;
     }
-    if (this.data.inputValue == "") {
-      wx.showToast({
-        title: '请输入姓名',
-        icon: "none",
-        duration: 2000
+  },
+  up() {
+    if (this.data.count<10) {
+      var count2 = this.data.count + 1;
+      var total2 = count2 * this.data.price
+      this.setData({
+        count: count2,
+        total: total2
       })
-      return;
     }
-    var id=this.data.arr._id;
-    console.log(id)
-    wx.navigateTo({
-      url: '/pages/cart/cart?id='+id,
-    })
-    db.collection('user').add({
-      data: {
-        name:this.data.inputValue,
-        phone:this.data.inputValue2,
-        id: this.data.arr._id
-      },
-      success: res => {console.log(res)},
-      fail: err => {},
-    })
-  },
-  bindInput: function (e) {
-    this.setData({
-      inputValue: e.detail.value
-    })
-  },
-  bindInput2: function (e) {
-    this.setData({
-      inputValue2: e.detail.value
-    })
   },
   /**
    * 生命周期函数--监听页面加载
@@ -61,12 +41,22 @@ Page({
     db.collection("ticket").where({
       _id: options.id
     }).get().then(res => {
-      console.log(res)
       this.setData({
-        arr: res.data[0]
+        result: res.data[0],
+        price: res.data[0].list[0].price2
       })
-    console.log(this.data.arr)
+      console.log(this.data.result)
     }).catch(err => { console.log(err) })
+
+    db.collection("user").where({
+      id: options.id
+    }).get().then(res => {
+      this.setData({
+        phone: res.data[0].phone
+      })
+      console.log(this.data.phone)
+    }).catch(err => { console.log(err) })
+
   },
 
   /**
--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -5,14 +5,56 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    //判断小程序的API，回调，参数，组件等是否在当前版本可用。
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    name:"",
+    src:"cloud://wangshuxin-78uzn.7761-wangshuxin-78uzn-1259662524/Icon_wode.png",
+    login:true,
+    show:false
   },
-
+  yes(){
+     setTimeout(function(){
+            wx.switchTab({
+              url: '/pages/main/main'
+            })
+          },1000)
+          this.setData({
+            name:"LOVEIS404NOTFOUND",
+            src:"cloud://wangshuxin-78uzn.7761-wangshuxin-78uzn-1259662524/mine.jpg",
+            login:false,
+            show:false
+          })
+  },
+  bindGetUserInfo: function (e) {
+    this.setData({
+      show:true
+    })
+    if (e.detail.userInfo) {
+     
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    // 查看是否授权
+    wx.getSetting({
+      success: function (res) {
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: function (res) {
+              //从数据库获取用户信息
+              that.queryUsreInfo();
+              //用户已经授权过
+              wx.switchTab({
+                url: ''
+              })
+            }
+          });
+        }
+      }
+    })
   },
 
   /**
